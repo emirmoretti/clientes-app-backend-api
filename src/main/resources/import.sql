453fe49443f3e5ed9421b9eaccd089db788cbcd0
emirmoretti <emir.moretti@hotmail.com.ar>
2022-09-15 00:44:05 -0300

    data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -31,5 +31,3 @@ INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
 INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1,1);
 
 INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2,2);
-
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2,1);
